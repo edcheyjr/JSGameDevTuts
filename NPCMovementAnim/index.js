c95bbd9a566344971e6e3c0936145c80480adbe4
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-04-16 15:28:30 +0300

    #5[NPCMovementAnim]: added optioning functionality

--- NPCMovementAnim/index.js
+++ NPCMovementAnim/index.js
@@ -5,29 +5,78 @@
  * @type {HTMLCanvasElement}
  */
 
+const Enemies = ['spike', 'bat', 'bat2', 'ghost']
+
 const canvas = document.getElementById('canvas1')
+const select = document.querySelector('#npc')
 const ctx = canvas.getContext('2d')
+// console.log('select', select)
+// events
+let enemyState = Enemies[0]
+let enemiesArray = []
+let isAnimating = true
+
+select.addEventListener('change', (e) => {
+  enemyState = e.target.value
+})
 
 CANVAS_WIDTH = canvas.width = 500
 CANVAS_HEIGHT = canvas.height = 1000
 const numberOfEnemies = 100
-const enemiesArray = []
 let gameFrame = 0
-
-const enemyImage = new Image()
-
 for (let i = 0; i < numberOfEnemies; i++) {
-  enemiesArray.push(new SpikeEnemy(enemyImage))
+  enemiesArray.push(new SpikeEnemy(new Image(), Enemies[0]))
+  enemiesArray.push(new BatEnemy(new Image(), Enemies[1]))
+  enemiesArray.push(new BatTwoEnemy(new Image(), Enemies[2]))
+  enemiesArray.push(new GhostEnemy(new Image(), Enemies[3]))
 }
 
-function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  enemiesArray.forEach((enemy) => {
-    enemy.draw(ctx)
-    enemy.update(gameFrame)
-  })
-  gameFrame++
-  requestAnimationFrame(animate)
+console.log('enemiesArray', enemiesArray)
+isAnimating = true
+if (Enemies.includes(enemyState)) {
+  isAnimating = true
 }
+if (isAnimating) {
+  function animate() {
+    // canvas.height = 1000
+    // canvas.width = 500
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    if (enemyState == Enemies[0]) {
+      enemiesArray
+        .filter((enemy) => enemy.name == Enemies[0])
+        .forEach((enemy) => {
+          enemy.draw(ctx)
+          enemy.update(gameFrame)
+        })
+    } else if (enemyState == Enemies[1]) {
+      enemiesArray
+        .filter((enemy) => enemy.name == Enemies[1])
+        .forEach((enemy) => {
+          enemy.draw(ctx)
+          enemy.update(gameFrame)
+        })
+    } else if (enemyState == Enemies[2]) {
+      enemiesArray
+        .filter((enemy) => enemy.name == Enemies[2])
+        .forEach((enemy) => {
+          enemy.draw(ctx)
+          enemy.update(gameFrame)
+        })
+    } else if (enemyState == Enemies[3]) {
+      enemiesArray
+        .filter((enemy) => enemy.name == Enemies[3])
+        .forEach((enemy) => {
+          enemy.draw(ctx)
+          enemy.update(gameFrame)
+        })
+    } else {
+      isAnimating = false
+      console.log('Not valid')
+    }
 
-animate()
+    gameFrame++
+    requestAnimationFrame(animate)
+  }
+
+  animate()
+}
