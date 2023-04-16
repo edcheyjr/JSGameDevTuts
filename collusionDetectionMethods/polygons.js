a2fa319f343447181506bfcc720777e210610169
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-04-16 13:47:11 +0300

    #4[collusionDetectionMethods]:skeleton class of polygons

---
+++ collusionDetectionMethods/polygons.js
@@ -0,0 +1,13 @@
+// comming soon
+
+class Polygon {
+  MAX_VERTICES = 30
+  constructor(noOfVertices = 5, color = '#000') {
+    this.noOfVertices = noOfVertices
+    this.color = color
+    this.MaxVertices = this.MAX_VERTICES
+  }
+  update() {}
+  draw() {}
+  checkIfCollided(polygon) {}
+}
